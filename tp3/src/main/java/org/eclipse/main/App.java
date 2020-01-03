0535c9a7e802a7b5dac87271143eaa4ba12031b8
el mouelhi <elmouelhi.achref@gmail.com>
2020-01-03 15:15:22 +0100

    authentification

--- tp3/src/main/java/org/eclipse/main/App.java
+++ tp3/src/main/java/org/eclipse/main/App.java
@@ -1,21 +1,21 @@
 package org.eclipse.main;
 
 import org.eclipse.dao.UtilisateurDaoImpl;
+import org.eclipse.frame.ConnexionFrame;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        UtilisateurDaoImpl utilisateurDaoImpl = new UtilisateurDaoImpl();
-        var utilisateurs = utilisateurDaoImpl.getAll();
-        utilisateurs.forEach(System.out::println);
-        System.out.println("---------------");
-        System.out.println(utilisateurDaoImpl.findByNomAndPrenom("wick", "john"));
-        System.out.println(utilisateurDaoImpl.findByNomAndPrenom("tyson", "mike"));
-    }
+public class App {
+	public static void main(String[] args) {
+		ConnexionFrame connexionFrame = new ConnexionFrame();
+//        System.out.println( "Hello World!" );
+//        UtilisateurDaoImpl utilisateurDaoImpl = new UtilisateurDaoImpl();
+//        var utilisateurs = utilisateurDaoImpl.getAll();
+//        utilisateurs.forEach(System.out::println);
+//        System.out.println("---------------");
+//        System.out.println(utilisateurDaoImpl.findByNomAndPrenom("wick", "john"));
+//        System.out.println(utilisateurDaoImpl.findByNomAndPrenom("tyson", "mike"));
+	}
 }
